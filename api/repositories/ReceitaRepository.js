de509d27ec36f937d392d014c7717da80fb13b0a
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-06-17 15:57:23 -0300

    ft: add recipe repository

---
+++ api/repositories/ReceitaRepository.js
@@ -0,0 +1,31 @@
+const { collection, getDocs } = require("firebase/firestore");const db = require("./firebaseConfig");
+const db = require("./firebaseConfig");
+
+class ReceitaRepository {
+    constructor() {
+        this.collectionRef = collection(db, "receita");
+    }
+
+    async listarTodas() {
+        try {
+            const snapshot = await getDocs(this.collectionRef);
+            const receitas = [];
+            snapshot.forEach((doc) => {
+                receitas.push({
+                    id: doc.id,
+                    ...doc.data()
+                });
+            });
+
+            return receitas;
+
+        } catch (error) {
+            console.log("Erro ao listar receitas:", error);
+            throw error;
+        }
+    }
+}
+
+
+
+module.exports = new ReceitaRepository();
