c17b28f6b065dddcb9433b1b04e80677c890a4e4
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-06-17 15:38:45 -0300

    ft: add recipe model

---
+++ api/models/Receita.js
@@ -0,0 +1,13 @@
+
+class ReceitaModel {
+    constructor(id, titulo, descricao, tempoPreparo, imagem){
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.tempoPreparo = tempoPreparo;
+        this.imagem = imagem;
+
+    }
+}
+
+module.exports = ReceitaModel;
