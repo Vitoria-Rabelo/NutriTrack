e3f8cef5e2517dfb210ba0af6aac148a8caf2243
Vitória Rabelo <104806771+Vitoria-Rabelo@users.noreply.github.com>
2026-06-17 16:21:02 -0300

    fix: update app.js

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const routerPaciente = require("./routers/PacienteRouter");
+const routerReceita = require("./routers/ReceitaRouter");
 
 const app = express();
 const port = 3000;
 
 app.use("/api/pacientes", routerPaciente);
+app.use("/api/receitas", routerReceita);
 
 app.use((request, response) => {
     response.status(404).json({ erro: "Rota inválida." });
@@ -15,4 +17,6 @@ app.listen(port, () => {
     console.log(`Rotas disponíveis:`);
     console.log(`- GET  http://localhost:${port}/api/pacientes`);
     console.log(`- POST http://localhost:${port}/api/pacientes`);
+    console.log(`- GET  http://localhost:${port}/api/receitas`);
+    console.log(`- GET  http://localhost:${port}/api/receitas?categoria=NomeDaCategoria`);
 });
